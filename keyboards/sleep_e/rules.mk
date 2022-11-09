538587f007d0546f8678cc53794b62608a16b01b
SleepyTrap <pascalraber@gmx.de>
2022-11-10 00:22:50 +0100

    sleep_e

---
+++ keyboards/sleep_e/rules.mk
@@ -0,0 +1,19 @@
+MCU = atmega32u4
+
+
+BOOTLOADER = atmel-dfu
+
+
+BOOTMAGIC_ENABLE = yes       # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no        # Console for debug
+COMMAND_ENABLE = no        # Commands for debug and configuration
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+NKRO_ENABLE = yes            # USB Nkey Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+MIDI_ENABLE = no            # MIDI support
+BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+AUDIO_ENABLE = no           # Audio output on port C6
+SPLIT_KEYBOARD = no
