613573c1f64d689742cf1b605dc5b043c449bb7e
SleepyTrap <pascalraber@gmx.de>
2022-12-27 22:22:58 +0100

    vial actually compiles

--- keyboards/sleep_e/keymaps/vial/config.h
+++ keyboards/sleep_e/keymaps/vial/config.h
@@ -3,5 +3,5 @@
 #pragma once
 
 #define VIAL_KEYBOARD_UID {0x41, 0x85, 0x44, 0xFF, 0x4E, 0x9C, 0xB5, 0x0C}
-#define VIAL_UNLOCK_COMBO_ROWS{0,1}
-#define VIAL_UNLOCK_COMBO_COLS{0,11}
+#define VIAL_UNLOCK_COMBO_ROWS {0,1}
+#define VIAL_UNLOCK_COMBO_COLS {0,11}
